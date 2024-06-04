ec528588a9c7ea62f5e796d5dd146122cf823e91
Naman Gupta <naman.mw4@gmail.com>
2024-06-05 00:41:24 +0530

    Change Social Icons

--- src/components/SocialShareButtons.jsx
+++ src/components/SocialShareButtons.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from 'react'
+
 import {
-  FaWhatsapp,
-  FaXTwitter,
-  FaFacebook,
+  FaSquareWhatsapp,
+  FaSquareXTwitter,
+  FaSquareReddit,
+  FaSquareFacebook,
   FaTelegram,
-  FaReddit,
 } from "react-icons/fa6";
 
 const SocialShareButtons = ({url, title}) => {
@@ -17,7 +18,7 @@ const SocialShareButtons = ({url, title}) => {
         rel="noreferrer"
         href={`https://api.whatsapp.com/send/?text=${url}`}
       >
-        <FaWhatsapp className="text-[#25D366] w-8 h-auto" />
+        <FaSquareWhatsapp className="text-[#25D366] w-8 h-auto" />
       </a>
 
       <a
@@ -25,21 +26,21 @@ const SocialShareButtons = ({url, title}) => {
         rel="noreferrer"
         href={`https://x.com/intent/tweet?url=${url}`}
       >
-        <FaXTwitter className="text-black w-8 h-auto" />
+        <FaSquareXTwitter className="text-black w-8 h-auto" />
       </a>
       <a
         target="_blank"
         rel="noreferrer"
         href={`https://www.reddit.com/submit?url=${url}&title=${title}`}
       >
-        <FaReddit className="text-[#FF4500] w-8 h-auto" />
+        <FaSquareReddit className="text-[#FF4500] w-8 h-auto" />
       </a>
       <a
         target="_blank"
         rel="noreferrer"
         href={`https://www.facebook.com/dialog/share?app_id=APP_ID&display=popup&href=${url}`}
       >
-        <FaFacebook className="text-[#3B5998] w-8 h-auto" />
+        <FaSquareFacebook className="text-[#3B5998] w-8 h-auto" />
       </a>
       <a target="_blank" rel="noreferrer" href="/">
         <FaTelegram className="text-[#0088CC] w-8 h-auto" />
