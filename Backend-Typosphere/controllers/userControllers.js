90dcf9d732a87ef751ec39b0335fce01bf768a5d
Naman Gupta <naman.mw4@gmail.com>
2024-06-11 00:18:21 +0530

    Add custom error handler

--- Backend-Typosphere/controllers/userControllers.js
+++ Backend-Typosphere/controllers/userControllers.js
@@ -1,5 +1,5 @@
 import User from '../models/User.js'
-export const registerUser = async (req, res) => {
+export const registerUser = async (req, res, next) => {
     try {
         const { name, email, password } = req.body;
 
@@ -7,7 +7,8 @@ export const registerUser = async (req, res) => {
         let user = await User.findOne({ email });
 
         if (user) {
-            return res.status(400).json({ message: "User have already registered" });
+            // return res.status(400).json({ message: "User have already registered" });
+            throw new Error("User have already registered");
         }
 
         // Creating a new user
@@ -25,8 +26,7 @@ export const registerUser = async (req, res) => {
             token: await user.generateJWT(),
         });
     } catch (error) {
-        return res.status(500).json({
-            message: "Something went wrong"
-        });
+        // return res.status(500).json({message: "Something went wrong"});
+        next(error);
     }
 };
