07229bf4f3a9ea2c51fd336448cbe16196e6fab1
Naman Gupta <naman.mw4@gmail.com>
2024-06-19 23:16:13 +0530

    Implement Redux
    Added react-redux and @reduxjs/toolkit to test the working of redux in the application. No feature is added in this commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import { Provider } from "react-redux";
+import store from "./store/index.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>
 );
