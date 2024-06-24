c53b88a8c64da23b78d6504a8a99bdd38ae80129
Naman Gupta <naman.mw4@gmail.com>
2024-06-24 18:55:57 +0530

    Create user actions
    Implement Logout functionality by clearing off the local storage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,9 @@ import { images } from "../constants";
 import { AiOutlineMenu } from "react-icons/ai";
 import { AiOutlineClose } from "react-icons/ai";
 import { RiArrowDropDownLine } from "react-icons/ri";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { logout } from "../store/actions/user";
 
 const NavItemsInfo = [
   { name: "Home", type: "link" },
@@ -69,13 +72,20 @@ const NavItem = ({ item }) => {
 };
 
 const Header = () => {
+  const dispatch = useDispatch();
   const [navIsVisible, setNavIsVisible] = useState(false);
+  const userState = useSelector((state) => state.user);
+  const [profileDropdown, setProfileDropdown] = useState(false);
 
   const navVisibilityHandler = () => {
     setNavIsVisible((currState) => {
       return !currState;
     });
   };
+
+  const logoutHandler = () => {
+    dispatch(logout());
+  }
 
   return (
     <section className="sticky top-0 left-0 right-0 z-50 bg-white">
@@ -121,9 +131,46 @@ const Header = () => {
               <NavItem key={item.name} item={item} />
             ))}
           </ul>
-          <button className="text-sm mt-5 lg:mt-0 border-2 border-primary px-6 py-2 rounded-full text-primary font-semibold hover:bg-primary hover:text-white transition-all duration-300">
-            Sign In
-          </button>
+          {userState.userInfo ? (
+            <div className="text-white text-base items-center gap-y-5 lg:text-dark-soft flex flex-col lg:flex-row gap-x-2 font-semibold">
+              <div className="relative group">
+                <div className="flex flex-col items-center">
+                  <button
+                    className="flex gap-x-1 items-center text-sm mt-5 lg:mt-0 border-2 border-primary px-6 py-2 rounded-full text-primary font-semibold hover:bg-primary hover:text-white transition-all duration-300"
+                    onClick={() => setProfileDropdown(!profileDropdown)}
+                  >
+                    <span>Profile</span>
+                    <RiArrowDropDownLine />
+                  </button>
+                  <div
+                    className={`${
+                      profileDropdown ? "block" : "hidden"
+                    } lg:hidden transition-all duration-500 pt-4 lg:absolute lg:bottom-0 lg:right-0 lg:transform lg:translate-y-full group-hover:block w-max`}
+                  >
+                    <ul className="bg-dark-hard lg:bg-transparent flex flex-col shadow-lg rounded-lg overflow-hidden">
+                      <button
+                        type="button"
+                        className="lg:hover:bg-dark-hard hover:bg-dark-soft hover:text-white px-4 py-2 text-white lg:text-dark-soft"
+                      >
+                        Dashboard
+                      </button>
+                      <button
+                        onClick={logoutHandler}
+                        type="button"
+                        className="lg:hover:bg-dark-hard hover:bg-dark-soft hover:text-white px-4 py-2 text-white lg:text-dark-soft"
+                      >
+                        Logout
+                      </button>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <button className="text-sm mt-5 lg:mt-0 border-2 border-primary px-6 py-2 rounded-full text-primary font-semibold hover:bg-primary hover:text-white transition-all duration-300">
+              Sign In
+            </button>
+          )}
         </div>
       </header>
     </section>
