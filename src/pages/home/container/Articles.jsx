fbf44b49eaba356bf838c78926f51947c2b06a2d
Naman Gupta <naman.mw4@gmail.com>
2024-06-24 18:26:14 +0530

    Create userReducer
    save user data in local storage

--- src/pages/home/container/Articles.jsx
+++ src/pages/home/container/Articles.jsx
@@ -2,16 +2,8 @@
 import React from "react";
 import { FaArrowRight } from "react-icons/fa";
 import ArticleCard from "../../../components/ArticleCard";
-import { useSelector, useDispatch } from "react-redux";
-import { changeCount } from "../../../store/actions/countActions";
 
 const Articles = () => {
-  const dispatch = useDispatch();
-  const count = useSelector((state) => state.count);
-
-  const countChangeHandler = (type) => {
-    dispatch(changeCount(type));
-  }
 
   return (
     <section className="flex flex-col container mx-auto px-5 py-10">
@@ -23,11 +15,6 @@ const Articles = () => {
       <button className="mx-auto flex items-center gap-x-2 font-bold text-primary border-2 border-primary px-6 py-3 rounded-lg">
         More Articles <FaArrowRight className="w-3 h-3" />
       </button>
-      <div className="mt-2 flex items-center gap-x-5">
-        <button onClick={() => countChangeHandler("DECREASE")}>decrease</button>
-        {count.number}
-        <button onClick={() => countChangeHandler("INCREASE")}>increase</button>
-      </div>
     </section>
   );
 };
--- src/pages/register/RegisterPage.jsx
+++ src/pages/register/RegisterPage.jsx
@@ -1,24 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MainLayout from "../../components/MainLayout";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { useMutation } from "@tanstack/react-query";
 import { signup } from "../../services/index/users";
 import toast from "react-hot-toast";
+import { useDispatch, useSelector } from "react-redux";
+import { userActions } from "../../store/reducers/userReducers";
 
 const RegisterPage = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const userState = useSelector((state) => state.user);
+
   const { mutate, isLoading } = useMutation({
     mutationFn: ({ name, email, password }) => {
       return signup({ name, email, password });
     },
     onSuccess: (data) => {
-      console.log(data);
+      dispatch(userActions.setUserInfo(data));
+      localStorage.setItem("account", JSON.stringify(data));
     },
     onError: (error) => {
       toast.error(`Error: ${error.message}`);
       console.log(error);
     },
   });
+
+  useEffect(() => {
+    if (userState.userInfo) {
+      navigate("/");
+    }
+  }, [navigate, userState.userInfo]);
+
   const {
     register,
     handleSubmit,
@@ -33,10 +47,12 @@ const RegisterPage = () => {
     },
     mode: "onChange",
   });
+
   const submitHandler = (data) => {
     const { name, email, password } = data;
     mutate({ name, email, password });
   };
+  
   const password = watch("password");
   return (
     <MainLayout>
