0502b01cac9f33964f42b83681e7d38f04dfd400
Naman Gupta <naman.mw4@gmail.com>
2024-06-26 18:07:18 +0530

    Fix: remove forgot password link

--- src/pages/register/RegisterPage.jsx
+++ src/pages/register/RegisterPage.jsx
@@ -192,16 +192,10 @@ const RegisterPage = () => {
                 </p>
               )}
             </div>
-            <Link
-              to="/forget-password"
-              className="text-sm font-semibold text-primary"
-            >
-              Forgot Password?
-            </Link>
             <button
               type="submit"
               disabled={!isValid || isLoading}
-              className="bg-primary text-white font-bold text-lg py-4 px-8 w-full rounded-lg my-6 disabled:opacity-70  disabled:cursor-not-allowed"
+              className="bg-primary text-white font-bold text-lg py-4 px-8 w-full rounded-lg mb-6 disabled:opacity-70  disabled:cursor-not-allowed"
             >
               Register
             </button>
