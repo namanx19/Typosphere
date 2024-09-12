f0a160ea750f6e6e1ba387b703fbc05afc55357b
Naman Gupta <naman.mw4@gmail.com>
2024-09-12 10:43:10 +0530

    Delete Post Endpoint

--- Backend-Typosphere/controllers/postControllers.js
+++ Backend-Typosphere/controllers/postControllers.js
@@ -78,4 +78,23 @@ const updatePost = async (req, res, next) => {
   }
 };
 
-export { createPost, updatePost };
+const deletePost = async (req, res, next) => {
+  try {
+    const post = await Post.findOneAndDelete({ slug: req.params.slug });
+
+    if (!post) {
+      const error = new Error("Post aws not found");
+      return next(error);
+    }
+
+    await Comment.deleteMany({ post: post._id });
+
+    return res.json({
+      message: "Post is successfully deleted",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { createPost, updatePost, deletePost };
--- Backend-Typosphere/models/Comment.js
+++ Backend-Typosphere/models/Comment.js
@@ -4,10 +4,10 @@ const CommentSchema = new Schema(
   {
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
     desc: { type: String, required: true },
-    postId: { type: Schema.Types.ObjectId, ref: "Post", required: true },
+    post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
     check: { type: Boolean, default: false },
-        parent: { type: Schema.Types.ObjectId, ref: "Comment", default: null },
-    replyOnUser: {type: Schema.Types.ObjectId, ref: "User", default: null}
+    parent: { type: Schema.Types.ObjectId, ref: "Comment", default: null },
+    replyOnUser: { type: Schema.Types.ObjectId, ref: "User", default: null },
   },
   { timestamps: true }
 );
