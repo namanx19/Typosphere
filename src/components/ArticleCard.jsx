6fefbc5260291c986d41ece36d5f81c81f7d8531
Naman Gupta <naman.mw4@gmail.com>
2024-09-13 07:55:30 +0530

    Add: Fetch all post and display on home page

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -4,39 +4,59 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { FaCheck } from "react-icons/fa";
-import { images } from "../constants";
+import { images, stables } from "../constants";
+import { AiOutlineClose } from "react-icons/ai";
 
-const ArticleCard = ({ className }) => {
+const ArticleCard = ({ post, className }) => {
   return (
     <div
       className={`rounded-xl overflow-hidden shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px] ${className}`}
     >
       <img
-        src={images.Post1Image}
+        src={
+          post.photo
+            ? stables.UPLOAD_FOLDER_BASE_URL + post.photo
+            : images.samplePostImage
+        }
         alt="post image"
         className="w-full object-cover object-center h-auto md:h-52 lg:h-48 xl:h-60"
       />
       <div className="p-5">
         <h2 className="font-courierprime font-bold text-xl text-dark-soft md:text-2xl lg:text-[28px]">
-          Future of Work
+          {post.title}
         </h2>
         <p className="text-dark-light mt-3 text-sm md:text-lg">
-          Majority of people will work in jobs that don't exist today.
+          {post.caption}
         </p>
         <div className="flex justify-between flex-nowrap items-center mt-6">
           <div className="flex items-center gap-x-2 md:gap-x-2.5">
             <img
-              src={images.PostProfile1}
+              src={
+                post.user.avatar
+                  ? stables.UPLOAD_FOLDER_BASE_URL + post.user.avatar
+                  : images.userImage
+              }
               alt="profile image"
-              className="w-9 h-9 md:w-10 md:h-10"
+              className="w-9 h-9 md:w-10 md:h-10 rounded-full"
             />
             <div className="flex flex-col">
               <h4 className="font-bold italic text-dark-soft text-sm md:text-base">
-                Viola Manisa
+                {post.user.name}
               </h4>
               <div className="flex items-center gap-x-2">
+                <span
+                  className={`${
+                    post.user.verified ? "bg-[#36B37E]" : "bg-red-500"
+                  } w-fit bg-opacity-20 p-1.5 rounded-full`}
+                >
+                  {post.user.verified ? (
+                    <FaCheck className="w-1.5 h-1.5 text-[#36B37E]" />
+                  ) : (
+                    <AiOutlineClose className="w-1.5 h-1.5 text-red-500" />
+                  )}
+                </span>
                 <span className="italic text-dark-light text-xs md:text-sm">
-                  Verified Writer
+                  {post.user.verified ? "Verified" : "Unverified"} writer
                 </span>
                 <span className="bg-[#36B37E] w-fit bg-opacity-20 p-1.5 rounded-full">
                   <FaCheck className="w-1.5 h-1.5 text-[#36b37E]" />
@@ -45,7 +65,10 @@ const ArticleCard = ({ className }) => {
             </div>
           </div>
           <span className="font-bold italic text-dark-light text-sm md:text-base">
-            02 May
+            {new Date(post.createdAt).getDate()}{" "}
+            {new Date(post.createdAt).toLocaleString("default", {
+              month: "long",
+            })}
           </span>
         </div>
       </div>
--- src/constants/images.js
+++ src/constants/images.js
@@ -5,6 +5,9 @@ import Post1Image from "../assets/Posts/post1.png";
 import PostProfile1 from "../assets/Posts/post-profile1.png";
 import CTAImage from "../assets/CtaImage.jpeg";
 
+const samplePostImage = "/images/sample.jpg";
+const userImage = "/images/user.png";
+
 const images = {
   Logo,
   Favicon,
@@ -12,6 +15,8 @@ const images = {
   Post1Image,
   PostProfile1,
   CTAImage,
+  samplePostImage,
+  userImage,
 };
 
 export default images;
