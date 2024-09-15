d060bdffdb933e16734b7314cc9ed6dcd0e92036
Naman Gupta <naman.mw4@gmail.com>
2024-09-15 17:25:45 +0530

    Add Article Card and Detail Skeleton for loading state

--- client/src/pages/articleDetail/ArticleDetailPage.jsx
+++ client/src/pages/articleDetail/ArticleDetailPage.jsx
@@ -20,6 +20,8 @@ import CommentsContainer from "../../components/comments/CommentsContainer";
 import SocialShareButtons from "../../components/SocialShareButtons";
 import { useQuery } from "@tanstack/react-query";
 import { getSinglePost } from "../../services/index/posts";
+import ArticleDetailSkeleton from "./components/ArticleDetailSkeleton";
+import ErrorMessage from "../../components/ErrorMessage";
 
 const postsData = [
   {
@@ -62,8 +64,8 @@ const ArticleDetailPage = () => {
   const { slug } = useParams();
   const [breadcrumbsData, setBreadCrumbsData] = useState([]);
   const [body, setBody] = useState(null);
-  const { data, isLoading, error } = useQuery({
-    queryKey: ['blog', slug],
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["blog", slug],
     queryFn: () =>
       getSinglePost({ slug }).then((res) => {
         setBreadCrumbsData([
@@ -89,52 +91,58 @@ const ArticleDetailPage = () => {
 
   return (
     <MainLayout>
-      <section className="container mx-auto max-w-5xl flex flex-col px-5 py-5 lg:flex-row lg:gap-x-5 lg:items-start">
-        <article className="flex-1">
-          <BreadCrumbs data={breadcrumbsData} />
-          <img
-            className="rounded-xl w-full"
-            src={
-              data?.photo
-                ? stables.UPLOAD_FOLDER_BASE_URL + data?.photo
-                : images.samplePostImage
-            }
-            alt={data?.title}
-          />
-          <div className="mt-4 flex gap-2">
-            {data?.categories.map((category) => (
-              <Link
-                to={`/blog?category=${category.name}`}
-                className="text-primary text-sm font-roboto inline-block md:text-base"
-              >
-                {category.name}
-              </Link>
-            ))}
-          </div>
-          <h1 className="text-xl font-medium font-courierprime mt-4 text-dark-hard md:text-[26px]">
-            {data?.title}
-          </h1>
-          <div className="mt-4 prose prose-sm sm:prose-base">{body}</div>
-          <CommentsContainer className="mt-10" loggedInUserId="a" />
-        </article>
-        <div>
-          <SuggestedPosts
-            header="Latest Articles"
-            posts={postsData}
-            tags={tagsData}
-            className="mt-8 lg:mt-0 lg:max-w-xs"
-          ></SuggestedPosts>
-          <div className="mt-7">
-            <h2 className="font-courierprime font-medium text-dark-hard mb-4 md:text-xl">
-              Share on:
-            </h2>
-            <SocialShareButtons
-              url={encodeURI("www.google.co.in")}
-              title={encodeURIComponent("Google")}
+      {isLoading ? (
+        <ArticleDetailSkeleton />
+      ) : isError ? (
+        <ErrorMessage message="Couldn't fetch the post details" />
+      ) : (
+        <section className="container mx-auto max-w-5xl flex flex-col px-5 py-5 lg:flex-row lg:gap-x-5 lg:items-start">
+          <article className="flex-1">
+            <BreadCrumbs data={breadcrumbsData} />
+            <img
+              className="rounded-xl w-full"
+              src={
+                data?.photo
+                  ? stables.UPLOAD_FOLDER_BASE_URL + data?.photo
+                  : images.samplePostImage
+              }
+              alt={data?.title}
             />
+            <div className="mt-4 flex gap-2">
+              {data?.categories.map((category) => (
+                <Link
+                  to={`/blog?category=${category.name}`}
+                  className="text-primary text-sm font-roboto inline-block md:text-base"
+                >
+                  {category.name}
+                </Link>
+              ))}
+            </div>
+            <h1 className="text-xl font-medium font-courierprime mt-4 text-dark-hard md:text-[26px]">
+              {data?.title}
+            </h1>
+            <div className="mt-4 prose prose-sm sm:prose-base">{body}</div>
+            <CommentsContainer className="mt-10" loggedInUserId="a" />
+          </article>
+          <div>
+            <SuggestedPosts
+              header="Latest Articles"
+              posts={postsData}
+              tags={tagsData}
+              className="mt-8 lg:mt-0 lg:max-w-xs"
+            ></SuggestedPosts>
+            <div className="mt-7">
+              <h2 className="font-courierprime font-medium text-dark-hard mb-4 md:text-xl">
+                Share on:
+              </h2>
+              <SocialShareButtons
+                url={encodeURI("www.google.co.in")}
+                title={encodeURIComponent("Google")}
+              />
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      )}
     </MainLayout>
   );
 };
