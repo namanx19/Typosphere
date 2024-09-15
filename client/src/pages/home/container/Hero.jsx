23bbe8c78c6c001eb14402b9582991487cc651db
Naman Gupta <naman.mw4@gmail.com>
2024-09-16 02:48:43 +0530

    Add Blog Search Bar and Code Refactor

--- client/src/pages/home/container/Hero.jsx
+++ client/src/pages/home/container/Hero.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { IoSearchOutline } from "react-icons/io5";
 import { images } from "../../../constants";
+import Search from "../../../components/Search";
 
 const Hero = () => {
   return (
@@ -16,19 +17,7 @@ const Hero = () => {
           faucibus turpis felis a mauris. Duis sodales nisl nec dolor tincidunt.
         </p>
 
-        <div className="flex flex-col gap-y-2.5 mt-10 lg:mt-6 xl:mt-10 relative">
-          <div className="relative">
-            <IoSearchOutline className="absolute left-3 top-1/2 -translate-y-1/2 w-6 h-6 text-[#959EAD]" />
-            <input
-              className="placeholder:font-bold font-semibold text-dark-soft placeholder:text-[#959EAD] placeholder:text-sm md:placeholder:text-base lg:placeholder:text-lg rounded-lg pl-12 pr-3 w-full py-3 focus:outline-none shadow-[0_20px_40px_rgba(8,_112,_184,_0.7)] md:py-4"
-              type="text"
-              placeholder="Search article"
-            />
-          </div>
-          <button className="w-full bg-primary text-white font-semibold rounded-lg px-5 py-3 md:absolute md:right-2 md:top-1/2 md:-translate-y-1/2 md:w-fit md:py-2">
-            Search
-          </button>
-        </div>
+        <Search className="mt-10 lg:mt-6 xl:mt-10" />
 
         <div className="flex mt-8 flex-col lg:items-start lg:flex-nowrap lg:gap-x-4 lg:mt-8">
           <span className="text-dark-light font-semibold italic mt-2 lg:mt-4 lg:text-sm xl:text-base">
